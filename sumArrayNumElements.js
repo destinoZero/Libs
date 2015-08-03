f425f2526bbafea3a76b760fa47b1579a5c986fd
Juan Chaur <juan.chaur89@gmail.com>
2015-08-03 11:23:29 +0200

    JC Adding namespace for the lib, refactor the initial function and create a similar one using reduce function

--- sumArrayNumElements.js
+++ sumArrayNumElements.js
@@ -1,21 +1,48 @@
-// Suma todos los elementos de un array numérico y devuelve el resultado.
-// Antes comprueba que los elementos del array son todos números.
-// Los índices que no sean numéricos son saltados y no se tienen en cuenta.
-function sumArrayNumElements(arr) {
-  var result = 0,
-      arrLen = arr.length,
-      elem, el;
+'use strict';
 
-  arr.forEach(function(elem) {
-    if(typeof elem !== 'number') {
-      el = elem;
-      console.log('"' + (typeof el) + '"' + ' is not a number.');
-    }
-    else {
-      result += elem;
-    }
-  });
+var dZlibs = dZlibs || {};
 
-  return result;
-}
+/**
+ * Suma todos los elementos de un array numerico y devuelve el resultado.
+ * Antes comprueba que los elementos del array son todos numeros
+ * Los indices que no sean numericos son saltados y no se tienen en cuenta
+ *
+ * @param  {Array} arr El array que quiere sumarse
+ * @return {Number}     Suma total de los numeros del array
+ */
+dZlibs.sumArrayNumElements = function(arr) {
+	var result = 0,
+		arrLen = arr.length,
+		elem
+	;
 
+	arr.forEach(function(elem) {
+		if(typeof elem !== 'number') {
+			console.log('"' + (typeof elem) + '"' + ' is not a number.');
+		} else {
+			result += elem;
+		}
+	});
+
+	return result;
+};
+
+/**
+ * Same as above
+ * @param  {Array} arr
+ * @return {Number}
+ */
+dZlibs.sumElementsInArray = function(arr) {
+
+	var result = arr.reduce(function(prevVal, currentVal) {
+
+		if (typeof currentVal === 'number') {
+			return prevVal + currentVal;
+		} else {
+			console.warn('Current element is not a Number: ', currentVal);
+			return prevVal;
+		}
+	});
+
+	return result;
+};
